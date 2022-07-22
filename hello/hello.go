65e56ed4feb3406c61bd4d898d7641df1adaea10
Sperber <C-Lily.Sperber@charter.com>
2022-07-22 15:01:15 -0400

    initCommit

---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	message := "Woah!!"
+	fmt.Println(message)
+}
---
+++ lissajous/lissajous.go
@@ -0,0 +1,29 @@
+// // Lissajous generates GIF animations of random Lissajous figures.
+// package main
+
+// import (
+// 	"image"
+// 	"image/color"
+// 	"image/gif"
+// 	"io"
+// 	"math"
+// 	"math/rand"
+// 	"os"
+// )
+
+// var pallete = []color.Color{color.White, color.Black}
+
+// const (
+// 	whiteIndex = 0
+// 	blackIndex = 1
+// )
+
+// func main() {
+// 	const (
+// 		cycles = 5 // number of complete x oscillator revolutions
+// 		res = 0.001 // angular resolution
+// 		size = 100 // image canvas covers [-size..+size]
+// 		nframes = 64 // number of animation frames
+// 		delay = 8 // delay between frames in 10ms units
+// 	)
+// }
